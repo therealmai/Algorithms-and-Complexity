ed942786c36b94ce083952cd487a47fada9b1dc3
Jomar Leano <jomar35leano@gmail.com>
2022-11-01 21:18:05 +0800

    done populating

--- Sorting Algorithms/Midterms/AVLTree.c
+++ Sorting Algorithms/Midterms/AVLTree.c
@@ -1,29 +1,46 @@
+//https://github.com/Frooge/3102N_Repo/blob/master/002midterm/001AVL.c
 #include<stdio.h>
+#include<stdlib.h>
+
+#define SIZE 8
 
 typedef struct node {
     int data;
     struct node* left;
     struct node* right;
-    int height;
-}AVLTree;
+}*AVLTree;
 
 
-void initTree(AVLTree* Tree);
-void populateTree(AVLTree *Tree);
+void initTree(AVLTree *T);
+void populateTree(AVLTree *T, int data[]);
+void insertNodeAVL(AVLTree node, AVLTree *T);
 
 int main(){
 
+    int data[SIZE] = {21,2,35,9,13,5,7,14};
     AVLTree T;
 
     initTree(&T);
-    populateTree(&T);
+    populateTree(&T, data);
 
     return 0;
 }
 
-void initTree(AVLTree* Tree){
-    Tree->height = -1;
-    Tree->left = NULL;
-    Tree->right = NULL;
-    Tree->data = 0;
+void initTree(AVLTree *T){
+    *T = NULL;
+}
+
+void populateTree(AVLTree* T, int data[]){
+    int x;
+    for(x=0; x<SIZE; x++){
+        AVLTree newNode = (struct node*)malloc(sizeof(struct node));
+        newNode->data = data[x];
+        newNode->left = NULL;
+        newNode->right = NULL;
+        insertNodeAVL(newNode, T);
+    }
+}
+
+void insertNodeAVL(AVLTree node, AVLTree *T){
+    return 0;
 }
