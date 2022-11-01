4b8178c32dd3ffd254bebcfc14b6adb4fca52c2b
Jomar Leano <jomar35leano@gmail.com>
2022-11-01 21:00:36 +0800

    done with initialize

--- Sorting Algorithms/Midterms/AVLTree.c
+++ Sorting Algorithms/Midterms/AVLTree.c
@@ -1,6 +1,29 @@
 #include<stdio.h>
 
+typedef struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+    int height;
+}AVLTree;
+
+
+void initTree(AVLTree* Tree);
+void populateTree(AVLTree *Tree);
+
 int main(){
 
+    AVLTree T;
+
+    initTree(&T);
+    populateTree(&T);
+
     return 0;
+}
+
+void initTree(AVLTree* Tree){
+    Tree->height = -1;
+    Tree->left = NULL;
+    Tree->right = NULL;
+    Tree->data = 0;
 }
