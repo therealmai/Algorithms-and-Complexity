47ea9bca09b42f88f7a5e10931704913c040ae32
Jomar Leaño <jomar35leano@gmail.com>
2022-12-09 09:53:14 +0800

    skip

--- Balancing Algorithms/Trees/skipList.c
+++ Balancing Algorithms/Trees/skipList.c
@@ -16,7 +16,9 @@ typedef struct {
 
 void initList(SkipList *list);
 void populateArr(SkipList* list, int arr[]);
-void insertSkip(SkipList* list, int arr[]);
+void insertSkip(SkipList* list, int data);
+Node updateInsert(Node head, int data, Node *update, int level);
+
 int main(){
     
     int arr[] = {21,2,35,9,13,5,7,14,99,0};
@@ -39,10 +41,38 @@ void initList(SkipList *list){
     list->head->link = (struct node**)calloc(sizeof(struct node*),maxLevel+1); 
 }
 
-void insertSkip(SkipList *list, int arr[]){
+void insertSkip(SkipList *list, int data){
     Node *update = (struct node**)calloc(sizeof(struct node*), maxLevel+1);
+    Node node = updateInsert(list->head, data, update, list->level); //traversal
+
+    if(node == NULL || node->data != data){
+        int newLevel = coinFlip();
+    }
 }
 
+int coinFlip(){
+
+}
+
+int coinFlip(){
+    int newLevel, coin;
+    for(newLevel = 0, coin = rand() % 2; coin && newLevel < maxLevel; newLevel++, coin = rand() % 2) {}
+    return newLevel;
+}
+
+Node updateInsert(Node head, int data, Node *update, int level){
+
+    if(head == NULL || head->data >= data){
+        return head;
+    }
+    update[level] = head;
+
+    if(head->link[level] != NULL && data >= head->link[level]->data || level == 0){
+        updateInsert(head->link[level], data, update, level); //move to the next link
+    }else{
+        updateInsert(head, data, update, level-1); //decrement level
+    }
+}
 
 void populateArr(SkipList* list, int arr[]){
     int x;
