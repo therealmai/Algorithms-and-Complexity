2e77f809c9ef622e6ead3b137310a387c3566cf4
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2022-12-11 19:14:32 +0800

    start of rod cutting

---
+++ Dynamic Programming/RodCutting.c
@@ -0,0 +1,18 @@
+/*
+Richard Bellman
+1950s
+-	Memoization is storing of answers to previously solved subproblems.
+-	Basically recursion
+Time Complexity
+O(n2) - in all cases
+Space Complexity
+O(n2) + O(n) worst case
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    return 0;
+}
