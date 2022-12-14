285697ee18a0831036ee70984807ff49c203a76e
Jomar Leaño <jomar35leano@gmail.com>
2022-12-14 21:05:37 +0800

    notes for redblack

--- Balancing Algorithms/Trees/redblacktrees.c
+++ Balancing Algorithms/Trees/redblacktrees.c
@@ -10,6 +10,22 @@ Red Property Node can only have Black Child
 Depth Property = must have the same number of Count Black in path
 New Node = Red
 
+RULES
+New nodes starts as red
+all NULL Nodes are black; have a nil node with elem=0, color=black, left=right=NULL to act as the null node
+Root is always black
+No consecutive reds
+
+Insertion
+Case 1: If the root node is red then change it to black 
+
+Case 2: Z uncle is red then recolor them which suffice the properties
+
+Case 3: Z uncle is black then do a avl rotation and match the color of parent and granparent which suffice the property
+
+Deletion
+
+
 Time complexity
 Amortized Access: O(log n) for accessing, searching and O(1) for insertions and deletion
 Worst: O(log n) in all cases
