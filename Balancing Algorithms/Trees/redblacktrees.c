0f0928ae53ce561293d94738a07ee092bb5cac35
Jomar Leaño <jomar35leano@gmail.com>
2022-12-14 21:21:53 +0800

    done with red black notes

--- Balancing Algorithms/Trees/redblacktrees.c
+++ Balancing Algorithms/Trees/redblacktrees.c
@@ -24,6 +24,23 @@ Case 2: Z uncle is red then recolor them which suffice the properties
 Case 3: Z uncle is black then do a avl rotation and match the color of parent and granparent which suffice the property
 
 Deletion
+Instance 1
+Case 1: If to be deleted is leaf just free or if to be deleted has one child then promote the child and color it to black
+Case 2: If to be deleted has 2 childs, exhange the value to its inorder predecessor then delete it (based it on case 1)
+
+Instance 2
+Case 1: If sibling of deleted value is black and has child red then do a simple avl rotation then recolor the child of sibling and 
+grandparent to black then color parent the former color of granparent
+
+Case 2: If sibling of deleted value is black and has black child. Color sibling to red if its parent is red then
+recolor parent to black. If parent is black already then make it double black
+
+Case 3: If sibling of deleted value is red then make the deleted value to be double black then do a rotation where sibling becomes
+the granparent (make them linear) and make it black while the former parent becomes red
+
+
+
+
 
 
 Time complexity
